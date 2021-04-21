f4c11711eda5f9b4cb4104e7c83e27e43565fffa
pablo <pablopaparini@hotmail.com>
2021-04-21 17:08:59 +0200

    adding secure log

--- app/src/main/java/com/pcordoba/speechtotexttest1/RestPoster.java
+++ app/src/main/java/com/pcordoba/speechtotexttest1/RestPoster.java
@@ -74,7 +74,7 @@ public class RestPoster {
             public Map<String, String> getHeaders() throws AuthFailureError {
                 Map<String, String> headers = new HashMap<>();
                 headers.put("Content-Type", "application/json");
-                headers.put("authorization", RestPoster.generateToken());
+                headers.put("authorization", SignUtil.generateToken());
                 return headers;
             }
         };
@@ -112,7 +112,7 @@ public class RestPoster {
         return jObject;
     }
 
-    private static char[] KEYSTORE_PASSWORD = "1234qwer".toCharArray();
+
     private SSLSocketFactory newSslSocketFactory() {
         try {
             // Get an instance of the Bouncy Castle KeyStore format
@@ -123,7 +123,7 @@ public class RestPoster {
             try {
                 // Initialize the keystore with the provided trusted certificates
                 // Provide the password of the keystore
-                trusted.load(in, KEYSTORE_PASSWORD);
+                trusted.load(in, SignUtil.KEYSTORE_PASSWORD);
             } finally {
                 in.close();
             }
@@ -148,7 +148,7 @@ public class RestPoster {
 
             String kmAlg = KeyManagerFactory.getDefaultAlgorithm();
             KeyManagerFactory kmf = KeyManagerFactory.getInstance(kmAlg);
-            kmf.init(trusted, KEYSTORE_PASSWORD);
+            kmf.init(trusted, SignUtil.KEYSTORE_PASSWORD);
 
             SSLContext context = SSLContext.getInstance("TLS");
             context.init(kmf.getKeyManagers(), trustAllCerts, new SecureRandom());
@@ -193,87 +193,6 @@ public class RestPoster {
         MainActivity.statusListAdapter.addAll(list);
         TextView capturedVoiceCmd = (TextView) ((Activity) context).findViewById(R.id.resultMessage);
         capturedVoiceCmd.setText(executedCmd);
-    }
-
-    private static final int[][] strategy = new int[][]{
-            {9, 9, 2, 4, 2, 3}, {1, 6, 3, 9, 5, 7}, {8, 7, 8, 3, 5, 9}, {9, 9, 9, 9, 9, 6}, {4, 6, 1, 1, 2, 5},
-            {2, 5, 7, 5, 3, 9}, {5, 5, 5, 8, 9, 6}, {6, 7, 2, 9, 8, 9}, {9, 2, 7, 6, 4, 3}, {9, 3, 9, 2, 3, 4},
-            {4, 1, 2, 3, 3, 4}, {4, 4, 7, 5, 9, 8}, {1, 6, 6, 6, 8, 2}, {5, 9, 5, 7, 9, 4}, {1, 5, 9, 8, 5, 8},
-            {8, 5, 4, 2, 8, 2}, {9, 7, 7, 1, 9, 8}, {4, 7, 7, 4, 6, 5}, {4, 7, 8, 8, 6, 1}, {6, 3, 1, 4, 9, 6},
-            {9, 9, 1, 8, 9, 6}, {2, 5, 2, 4, 3, 6}, {8, 8, 2, 1, 2, 7}, {6, 8, 1, 5, 4, 8}, {2, 7, 3, 4, 1, 6},
-            {4, 3, 4, 3, 6, 4}, {3, 9, 3, 9, 3, 5}, {7, 5, 2, 6, 4, 4}, {9, 5, 7, 5, 2, 8}, {9, 3, 9, 8, 6, 3},
-            {6, 4, 5, 7, 6, 8}, {4, 2, 9, 4, 8, 5}, {4, 8, 8, 3, 6, 9}, {4, 7, 3, 1, 9, 8}, {5, 3, 8, 1, 9, 7},
-            {7, 2, 4, 5, 8, 8}, {3, 3, 7, 8, 4, 9}, {5, 5, 1, 4, 1, 4}, {1, 7, 8, 1, 7, 8}, {6, 1, 2, 6, 7, 6},
-            {1, 3, 6, 3, 9, 7}, {3, 6, 2, 6, 4, 2}, {4, 6, 2, 8, 5, 1}, {9, 7, 2, 2, 8, 6}, {7, 1, 7, 9, 1, 8},
-            {6, 3, 1, 5, 8, 8}, {9, 3, 7, 4, 1, 9}, {1, 1, 8, 2, 3, 7}, {1, 7, 2, 7, 5, 8}, {6, 1, 4, 3, 8, 5},
-            {6, 3, 9, 4, 6, 5}, {7, 5, 7, 8, 9, 2}, {1, 7, 9, 9, 9, 8}, {2, 1, 1, 2, 4, 2}, {6, 7, 6, 1, 9, 2},
-            {8, 3, 8, 4, 6, 3}, {6, 7, 4, 2, 5, 7}, {9, 6, 9, 6, 8, 3}, {6, 2, 9, 5, 5, 2}, {9, 1, 2, 6, 1, 5},
-            {7, 5, 6, 7, 6, 9}, {7, 2, 4, 2, 9, 7}, {4, 4, 5, 4, 6, 8}, {9, 5, 8, 5, 3, 4}, {7, 5, 7, 2, 3, 6},
-            {6, 6, 8, 3, 3, 7}, {9, 4, 5, 1, 6, 7}, {8, 4, 8, 5, 3, 9}, {8, 4, 4, 7, 3, 8}, {5, 8, 2, 1, 5, 1},
-            {2, 7, 1, 1, 9, 2}, {7, 4, 2, 5, 1, 3}, {3, 8, 2, 3, 4, 6}, {6, 8, 2, 8, 8, 5}, {4, 8, 1, 9, 2, 4},
-            {2, 4, 8, 8, 2, 4}, {3, 2, 8, 1, 4, 3}, {9, 2, 8, 2, 9, 5}, {5, 1, 5, 5, 9, 6}, {8, 1, 5, 9, 8, 4},
-            {4, 3, 5, 5, 4, 9}, {6, 2, 1, 4, 5, 2}, {4, 8, 7, 9, 6, 1}, {5, 6, 1, 3, 3, 2}, {6, 2, 1, 7, 5, 9},
-            {4, 1, 5, 6, 6, 6}, {1, 4, 5, 3, 3, 7}, {9, 4, 9, 8, 7, 7}, {5, 7, 4, 5, 2, 8}, {4, 7, 1, 4, 7, 1},
-            {5, 5, 4, 3, 9, 9}, {7, 3, 4, 5, 7, 7}, {9, 4, 5, 8, 4, 8}, {7, 4, 8, 8, 7, 1}, {1, 9, 1, 3, 3, 2},
-            {1, 1, 6, 7, 1, 8}, {1, 2, 7, 1, 1, 3}, {9, 9, 5, 3, 2, 4}, {3, 2, 6, 3, 7, 6}, {7, 6, 2, 1, 2, 4}
-    };
-
-    private static final String scrambledNumbers = "7925163804";
-    private static final String scrambledLetters = "gNQnPFpwRMhsvBWrJySOfxUDjHelCXAGaqtzEkuZKIdcVobLmYiT";
-    private static final String letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-
-    private static String generateToken() {
-
-        Calendar rightNow = Calendar.getInstance();
-        int hour = rightNow.get(Calendar.HOUR_OF_DAY);
-        int minute = rightNow.get(Calendar.MINUTE);
-        int day = rightNow.get(Calendar.DATE);
-        int month = rightNow.get(Calendar.MONTH) + 1;
-        int year = Integer.parseInt(Integer.toString(rightNow.get(Calendar.YEAR)).substring(2));
-        int dayOfYear = rightNow.get(Calendar.DAY_OF_YEAR);
-
-        Random random = new Random();
-        int ran = random.nextInt(100);
-        System.out.println(ran);
-
-        int[] card = strategy[ran];
-        int result = (hour + card[0]) * (minute + card[1]) * (day + card[2]) * (month + card[3])
-                * (year + card[4]) * (dayOfYear + card[5]);
-        int partial = result - card[0] - card[2] - card[5];
-        String ranString = Integer.toString(ran);
-        String pre, post;
-        if (ranString.length() == 1) {
-            pre = "H";
-            post = ranString;
-        } else {
-            pre = String.valueOf(ranString.charAt(0));
-            post = String.valueOf(ranString.charAt(1));
-
-        }
-
-        String token = pre + Integer.toString(partial).replaceAll("0", "X") + post;
-
-        byte[] data = new byte[0];
-        try {
-            data = token.getBytes("UTF-8");
-        } catch (UnsupportedEncodingException e) {
-            return "tokenError";
-        }
-
-        String baseToken = Base64.encodeToString(data, Base64.DEFAULT);
-
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < baseToken.length(); i++) {
-            char ch = baseToken.charAt(i);
-            if (Character.isDigit(ch)){
-                sb.append(scrambledNumbers.charAt(Character.getNumericValue(ch)));
-            } else if (Character.isLetter(ch)){
-                int index = letters.indexOf(ch);
-                sb.append(scrambledLetters.charAt(index));
-            }
-        }
-
-        return sb.toString();
     }
 
 }
